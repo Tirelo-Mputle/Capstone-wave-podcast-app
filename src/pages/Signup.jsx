1d28e8ab1c38b679d79a69be080d3138af78e746
Tirelo-Mputle <tirelomputle0@gmail.com>
2023-07-25 14:40:03 +0200

    Created home and signup page.
    Added react-router-dom

---
+++ src/pages/Signup.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+const Signup = () => {
+  return (
+    <>
+      <h1>Signup</h1>
+      <Link to="/">To home page</Link>
+    </>
+  );
+};
+
+export default Signup;
